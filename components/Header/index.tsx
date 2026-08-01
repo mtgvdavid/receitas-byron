f54ccdc709b779a599a99cd7c1c05f1f2ef8e729
Matheus David <d2026003904@unifei.edu.br>
2026-08-01 10:39:54 -0300

    feat:estrutura header

---
+++ components/Header/index.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"    
+export default function Header(){
+    return(
+        <header>
+            <Link href="/">Receitas deliciosas</Link>
+            <nav>
+                <Link href="/">
+                    Início
+                </Link>
+                <Link href="/receitas">
+                    Receitas
+                </Link>
+            </nav>
+        </header>
+    )
+}
