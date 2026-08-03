025e949f7d43b525715deadbc210dc022021f935
Matheus David <d2026003904@unifei.edu.br>
2026-08-03 14:46:15 -0300

    estrutura home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
 export default function Home() {
   return (
-    <div>
-      <h1>Tela Inicial</h1>
+    <div className="container mx-auto">
+
+{/* HERO */}
+      <section>
+        <h1>Receitas deliciosas</h1>
+        <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
+        <Link href="/receitas">
+          Ver todas as receitas
+        </Link>
+      </section>
+
     </div>
   );
 }
