a688d073c7637f0de5feac3953509d673db8299b
Matheus David <d2026003904@unifei.edu.br>
2026-08-03 18:22:58 -0300

    estilizacao do hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    
+    <main className="grow">
+      {/* HERO */}
+      <section className=" bg-orange-50 py-12">
+        <div className="flex flex-col items-center gap-6 container mx-auto">
 
-{/* HERO */}
-      <section>
-        <h1>Receitas deliciosas</h1>
-        <p>Descubra receitas simples e saborosas para todas as ocasiões</p>
-        <Link href="/receitas">
+        <h1 className="text-5xl font-bold">Receitas deliciosas</h1>
+        <p className="text-xl">Descubra receitas simples e saborosas para todas as ocasiões</p>
+        <Link className="bg-orange-500 hover:scale-105 transition-all text-white font-bold rounded-lg px-3 py-2" href="/receitas">
           Ver todas as receitas
         </Link>
-      </section>
 
-    </div>
+        </div>
+      </section>
+    </main>
+
+
   );
 }
