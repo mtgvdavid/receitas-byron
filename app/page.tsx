bf719a76eef253d3f1f0d08930823b04d8bc38dc
Matheus David <d2026003904@unifei.edu.br>
2026-08-04 09:09:04 -0300

    estrutura pagina receitas

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <section className="py-12">
         <div className="flex flex-col items-center container mx-auto">
         <h2 className="text-lg font-bold">Receitas em destaque</h2>
-        {/*cards de receita*/}
+        {/*TODO: cards de receita*/}
 
         <Link className="flex hover:text-orange-700 transition-colors text-orange-400" href="/receitas">Ver todas as receitas <ChevronRight /> </Link>
         </div>
---
+++ app/receitas/page.tsx
@@ -0,0 +1,12 @@
+export default function ReceitasPage(){
+    return(
+        <main className="grow py-8">
+            <div className="container mx-auto">
+                <h1 className="text-3xl font-bold">Todas as receitas</h1>
+                <div>
+                    {/*array de cards de receitas*/}
+                </div>
+            </div>
+        </main>
+    )
+}
